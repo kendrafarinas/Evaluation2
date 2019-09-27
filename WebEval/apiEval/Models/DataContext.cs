b29ae9a43fe1998c5ccf89618639ba76c5cab9bd
visual_community@outlook.com <visual_community@outlook.com>
2019-09-27 18:27:43 -0400

    api

---
+++ WebEval/apiEval/Models/DataContext.cs
@@ -0,0 +1,17 @@
+﻿
+namespace apiEval.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<apiEval.Models.Estudiante> Estudiantes { get; set; }
+
+        public System.Data.Entity.DbSet<apiEval.Models.Nota> Notas { get; set; }
+    }
+}
